f00fae2eb77baeb871de8aeb42e80292d727bc3b
Emerson Aguilar <emerson.aguilar@gmail.com>
2021-09-18 14:27:47 -0600

    Metodos de Leer, Crear y Actualizar equipos

--- apiPrestamos/prestasmosApi/Controllers/equiposController.cs
+++ apiPrestamos/prestasmosApi/Controllers/equiposController.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using prestasmosApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace prestasmosApi.Controllers
 {
@@ -13,15 +15,110 @@ namespace prestasmosApi.Controllers
             this._contexto = miContexto;
         }
 
+        /// <summary>
+        /// Metodo para retornar todos los reg. de la tabla de EQUIPOS
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [Route("api/equipos")]
         public IActionResult Get(){
-            var equiposList = _contexto.equipos;
-            if(equiposList.Count>0){
+            IEnumerable<equipos> equiposList = from e in _contexto.equipos
+                                               select e;
+            if (equiposList.Count() > 0)
+            {
                 return Ok(equiposList);
-            }
-            return NotFound();            
+            }      
+            return NotFound();              
         } 
+
+        /// <summary>
+        /// Metodo para retornar UN reg. de la tabla EQUIPOS filtrado por ID
+        /// </summary>
+        /// <param name="id">Valor Entero del campo id_equipos</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/equipos/{id}")]
+        public IActionResult getbyId(int id)
+        {
+            equipos unEquipo = (from e in _contexto.equipos
+                                where e.id_equipos == id //Filtro por ID
+                                select e).FirstOrDefault();
+            if( unEquipo != null)
+            {
+                return Ok(unEquipo);
+            }
+
+            return NotFound();
+        }
+
+
+        /// <summary>
+        /// Este metodo retorna los reg. en la tabl de EQUIPOS que contengan el valor dado en el parametro.
+        /// </summary>
+        /// <param name="buscarNombre"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/equipo/buscarnombre/{buscarNombre}")]
+        public IActionResult obtenerNombre(string buscarNombre)
+        {
+            IEnumerable<equipos> equipoPorNombre = from e in _contexto.equipos
+                                                   where e.nombre.Contains(buscarNombre)
+                                                   select e;
+            if (equipoPorNombre.Count() > 0)
+            {
+                return Ok(equipoPorNombre);
+            }
+
+            return NotFound();
+        }
+
+
+        [HttpPost]
+        [Route("api/equipos")]
+        public IActionResult guardarEquipo([FromBody] equipos equipoNuevo)
+        {
+            try
+            {
+                IEnumerable<equipos> equipoExiste = from e in _contexto.equipos
+                                                    where e.nombre == equipoNuevo.nombre
+
+                                                    select e;
+                if(equipoExiste.Count()==0)
+                {
+                    _contexto.equipos.Add(equipoNuevo);
+                    _contexto.SaveChanges();
+                    return Ok(equipoNuevo);
+                }
+                return Ok(equipoExiste);
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        [Route("api/equipos")]
+        public IActionResult updateEquipo([FromBody] equipos equipoAModificar)
+        {
+            equipos equipoExiste = (from e in _contexto.equipos
+                                    where e.id_equipos == equipoAModificar.id_equipos
+                                    select e).FirstOrDefault();
+            if(equipoExiste is null)
+            {
+                return NotFound();
+            }
+
+            equipoExiste.nombre = equipoAModificar.nombre;
+            equipoExiste.descripcion = equipoAModificar.descripcion;
+            equipoExiste.modelo = equipoAModificar.modelo;
+
+            _contexto.Entry(equipoExiste).State = EntityState.Modified;
+            _contexto.SaveChanges();
+
+            return Ok(equipoExiste);
+
+        }
 
     }
 }
